83dc5f7bcc5fab2c83d49cc86f10ee6740a102d3
waifuNET <artem228.tyg@gmail.com>
2024-07-10 20:40:26 +0500

    outline disabled

--- Assets/Core/Scripts/Iteract/IRLButton_View.cs
+++ Assets/Core/Scripts/Iteract/IRLButton_View.cs
@@ -7,41 +7,39 @@ using DG.Tweening;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
-[RequireComponent(typeof(Outline))]
 public class IRLButton_View : MonoBehaviour
 {
-	public Outline outline;
 	public bool active = true;
-	private IEnumerator smothIE;
+	//private IEnumerator smothIE;
 	private void Start()
 	{
-		outline = GetComponent<Outline>();
+		//outline = GetComponent<Outline>();
 	}
 
 	public void Select(bool select)
 	{
-		if (active)
-		{
-			if(smothIE != null) StopCoroutine(smothIE);
-			smothIE = SmoothOutlineWidth(select ? 10 : 0);
-			StartCoroutine(smothIE);
-		}
+		//if (active)
+		//{
+		//	if(smothIE != null) StopCoroutine(smothIE);
+		//	smothIE = SmoothOutlineWidth(select ? 10 : 0);
+		//	StartCoroutine(smothIE);
+		//}
 	}
-	private IEnumerator SmoothOutlineWidth(float need)
-	{
-		float smooth_base = 0.5f;
-		float smooth = outline.OutlineWidth > need ? smooth_base * -1 : smooth_base;
-		float _now_value = outline.OutlineWidth;
-		while (outline.OutlineWidth != need)
-		{
-			if(outline.OutlineWidth > 10 || _now_value < 0)
-			{
-				outline.OutlineWidth = need;
-				break;
-			}
-			_now_value += smooth;
-			outline.OutlineWidth = _now_value;
-			yield return new WaitForSeconds(0.01f);
-		}
-	}
+	//private IEnumerator SmoothOutlineWidth(float need)
+	//{
+	//	float smooth_base = 0.5f;
+	//	float smooth = outline.OutlineWidth > need ? smooth_base * -1 : smooth_base;
+	//	float _now_value = outline.OutlineWidth;
+	//	while (outline.OutlineWidth != need)
+	//	{
+	//		if(outline.OutlineWidth > 10 || _now_value < 0)
+	//		{
+	//			outline.OutlineWidth = need;
+	//			break;
+	//		}
+	//		_now_value += smooth;
+	//		outline.OutlineWidth = _now_value;
+	//		yield return new WaitForSeconds(0.01f);
+	//	}
+	//}
 }
