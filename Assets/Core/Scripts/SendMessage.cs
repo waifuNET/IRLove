7dc7ce58f30bdaef3e25c7cc8c81986bd20d4661
waifuNET <artem228.tyg@gmail.com>
2024-07-11 20:39:56 +0500

    player message fix

--- Assets/Core/Scripts/SendMessage.cs
+++ Assets/Core/Scripts/SendMessage.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,12 +6,18 @@ public class SendMessage : MonoBehaviour
 {
 	public TextMeshProUGUI textMeshProUGUI;
 	private IEnumerator IEstartVariable;
-	public void SendPlayerMessage(string text, Color color, float delay = 0.1f, bool bold = false, float clearTime = 5)
-    {
-		if(IEstartVariable != null) StopCoroutine(IEstartVariable);
+
+	public void SendPlayerMessage(string text, Color color, float delay = 0.1f, bool bold = false, float clearTime = 3)
+	{
+		if (textMeshProUGUI == null)
+			textMeshProUGUI = GameObject.FindGameObjectWithTag("PlayerMessageUI").GetComponent<TextMeshProUGUI>();
+
+		if (IEstartVariable != null)
+			StopCoroutine(IEstartVariable);
+
 		IEstartVariable = IEstart(text, color, delay, bold, clearTime);
 		StartCoroutine(IEstartVariable);
-    }
+	}
 
 	private IEnumerator IEstart(string text, Color color, float delay, bool bold, float clearTime)
 	{
@@ -20,11 +25,22 @@ public class SendMessage : MonoBehaviour
 		for (int i = 0; i < text.Length; i++)
 		{
 			textMeshProUGUI.text += text[i];
-			if (text[i] == ' ') continue;
+			if (text[i] == ' ')
+				continue;
+
 			yield return new WaitForSeconds(delay);
 		}
 
 		yield return new WaitForSeconds(clearTime);
-		textMeshProUGUI.SetText(" ");
+		textMeshProUGUI.text = " ";
+
+		// Принудительное обновление состояния
+		textMeshProUGUI.ForceMeshUpdate();
+		Canvas.ForceUpdateCanvases();
+
+		// Отключение и включение компонента
+		textMeshProUGUI.enabled = false;
+		yield return null;
+		textMeshProUGUI.enabled = true;
 	}
 }
