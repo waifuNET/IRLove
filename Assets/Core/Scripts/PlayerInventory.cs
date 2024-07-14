e94d25b695a523470200fd961a564107b523390a
DatiDizuDoneDatoChipu <130145070+DatiDizuDoneDatoChipu@users.noreply.github.com>
2024-07-14 23:03:10 +0300

    Player Inv Code Refactoring

--- Assets/Core/Scripts/PlayerInventory.cs
+++ Assets/Core/Scripts/PlayerInventory.cs
@@ -101,43 +101,9 @@ public class PlayerInventory : MonoBehaviour
         }
     }
     
-    private void ItemRotate()
-    {
-        Rigidbody origObjrg = CurrentItem.OriginalObject.GetComponent<Rigidbody>();
-        origObjrg.interpolation = RigidbodyInterpolation.None;
-        origObjrg.constraints = RigidbodyConstraints.FreezeRotationY;
-        Transform origObjTrans = CurrentItem.OriginalObject.transform;
-        if (Input.mouseScrollDelta.y != 0)
-        {
-            origObjTrans.Rotate(Vector3.up * Input.mouseScrollDelta.y * rotationIndex,Space.World);
-        }
-    }
-
     private bool _normalRotation = false;
-    private void DropAndPutButton()
-    {
-        if (timeButtonStart)
-        {
-            timeButton += Time.deltaTime;
-        }
-
-        if (CurrentItem.gameObject == null) return;
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.G))
-            {
-                timeButtonStart = true;
-                heldButton = true;
-                
-            }
-            else if (Input.GetKeyUp(KeyCode.G))
-            {
-                PutAndDropItem();
-            }
-        }
-        ItemRayCast();
-        DetectionNewItemPos();
-    }
+    
+    //RayCast
     private void ItemRayCast()
     {
         RaycastHit hit;
@@ -153,143 +119,6 @@ public class PlayerInventory : MonoBehaviour
         }
     }
 
-    private void PutAndDropItem()
-    {
-        RemoveGhostEffets(blueGhostMaterial);
-        RemoveGhostEffets(redGhostMaterial);
-        MakeItemReal();
-        timeButtonStart = false;
-        if (timeButton < heldTime)
-        {
-            itemDropped = true;
-            ItemDrop();
-            timeButton = 0;
-        }
-        else
-        {
-            if (putDistance < maxDistance)
-            {
-                if (!itemDropped)
-                {
-                    CreateBigWeight();
-                }
-                RemoveItem(CurrentItem);
-            }
-            else { Debug.Log("Too far"); CurrentItem.OriginalObject.SetActive(false); }
-            timeButton = 0;
-        }
-        itemDropped = false;
-        heldButton = false;
-    }
-    
-    private void CreateBigWeight()
-    {
-        Rigidbody rg = CurrentItem.OriginalObject.GetComponent<Rigidbody>();
-        rg.linearVelocity = Vector3.zero;
-        rg.angularDamping = 50;
-        rg.constraints = RigidbodyConstraints.None;
-        rg.interpolation = RigidbodyInterpolation.Interpolate;
-    }
-    private void DetectionNewItemPos()
-    {
-        if (heldButton && timeButton > heldTime)
-        {
-			if (!_normalRotation) CurrentItem.OriginalObject.transform.rotation =
-		CurrentItem.OriginalObject.GetComponent<IPickUpInfo>().GetRotation();
-			_normalRotation = true;
-
-			MakeItemGhost();
-            if (putDistance < maxDistance)
-            {
-                if (redAdd) { RemoveGhostEffets(redGhostMaterial); }
-                PutGhostEffect();
-                ItemPut();
-            }
-            else
-            {
-                if (blueAdd) { RemoveGhostEffets(blueGhostMaterial); }
-                PutGhostEffectOutOfRange();
-                ItemPut();
-            }
-        }
-    }
-
-    private void PutGhostEffect()
-    {
-        if (blueAdd) { return; } 
-        renderers = CurrentItem.OriginalObject.GetComponentsInChildren<Renderer>();
-        foreach (var renderer in renderers)
-        {
-            var materials = renderer.sharedMaterials.ToList();
-
-            materials.Add(blueGhostMaterial);
-            blueAdd = true;
-
-            renderer.materials = materials.ToArray();
-        }
-        blueAdd = true;
-
-    }
-    private void PutGhostEffectOutOfRange()
-    {
-        if (redAdd) { return; }
-        renderers = CurrentItem.OriginalObject.GetComponentsInChildren<Renderer>();
-        foreach (var renderer in renderers)
-        {
-            var materials = renderer.sharedMaterials.ToList();
-
-            materials.Add(redGhostMaterial);
-            redAdd = true;
-
-            renderer.materials = materials.ToArray();
-        }
-        redAdd = true;
-    }
-
-    private void RemoveGhostEffets(Material m)
-    {
-        if (!redAdd && !blueAdd) { return; }
-        foreach (var renderer in renderers)
-        {
-            var materials = renderer.sharedMaterials.ToList();
-
-            materials.Remove(m);
-            if(m == blueGhostMaterial) { blueAdd = false; }
-            if(m == redGhostMaterial) {  redAdd = false; }
-
-            renderer.materials = materials.ToArray();
-        }
-    }
-    
-    private void SetItemNewPos(Vector3 h)
-    {
-        if(CurrentItem.OriginalObject.GetComponent<BoxCollider>() != null)
-        {
-            itemNewPos = new Vector3(
-            h.x,
-            h.y,
-            h.z + CurrentItem.OriginalObject.GetComponent<BoxCollider>().size.z / 2f
-
-			);
-        }
-        else if(CurrentItem.OriginalObject.GetComponent<CapsuleCollider>() != null ) 
-        {
-            itemNewPos = new Vector3(
-            h.x,
-            h.y + CurrentItem.OriginalObject.GetComponent<CapsuleCollider>().radius,
-            h.z
-            );
-        }
-        else if (CurrentItem.OriginalObject.GetComponent<SphereCollider>() != null)
-        {
-            itemNewPos = new Vector3(
-            h.x,
-            h.y + CurrentItem.OriginalObject.GetComponent<SphereCollider>().radius,
-            h.z
-            );
-        }
-    }
-    
     public void ItemSwitch()
 	{
         if(inventoryItemScrollPosition > inventory.Count-1 || inventoryItemScrollPosition < 0)
@@ -319,44 +148,7 @@ public class PlayerInventory : MonoBehaviour
         item.Iterction();
     } 
 
-	private void ItemDrop()
-	{
-        GameObject origObj = CurrentItem.OriginalObject;
-        if (origObj.GetComponent<Rigidbody>() == null)
-        {
-            Rigidbody rr = origObj.gameObject.AddComponent<Rigidbody>();
-            rr.mass = 1;
-            rr.angularDamping = 1.75f;
-        }
-        Rigidbody rb = origObj.GetComponent<Rigidbody>();
-        rb.mass = 1;
-        rb.interpolation = RigidbodyInterpolation.Interpolate;
-        rb.constraints = RigidbodyConstraints.None;
-        rb.angularDamping = 1.75f;
-        origObj.transform.position = PlayerCamera.transform.position;
-        origObj.SetActive(true);
-        rb.AddForce(PlayerCamera.transform.TransformDirection(Vector3.forward), ForceMode.Impulse);
-        RemoveItem(CurrentItem);
-    }
-    private void MakeItemGhost()
-    {
-        Collider cd = CurrentItem.OriginalObject.GetComponent<Collider>();
-        cd.excludeLayers = ignoreLayer;
-        Rigidbody rb = CurrentItem.OriginalObject.GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-    }
-    private void MakeItemReal()
-    {
-        Collider cd = CurrentItem.OriginalObject.GetComponent<Collider>();
-        cd.excludeLayers = normLayer;
-    }
-    private void ItemPut()
-    {
-        SetItemNewPos(tempPos);
-        GameObject origObj = CurrentItem.OriginalObject;
-        origObj.transform.position = itemNewPos;
-        origObj.SetActive(true);
-    }
+	
     private void ChangeItem(ItemInterface item)
     {
 		if (item == null) return;
@@ -374,5 +166,225 @@ public class PlayerInventory : MonoBehaviour
         inventoryItemScrollPosition -= 1;
         ItemSwitch();
         inventory.Remove(item);
+    }
+
+    //Ghost Effects Shaders
+    private void PutGhostEffect()
+    {
+        if (blueAdd) { return; }
+        renderers = CurrentItem.OriginalObject.GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            var materials = renderer.sharedMaterials.ToList();
+
+            materials.Add(blueGhostMaterial);
+            blueAdd = true;
+
+            renderer.materials = materials.ToArray();
+        }
+        blueAdd = true;
+
+    }
+    private void PutGhostEffectOutOfRange()
+    {
+        if (redAdd) { return; }
+        renderers = CurrentItem.OriginalObject.GetComponentsInChildren<Renderer>();
+        foreach (var renderer in renderers)
+        {
+            var materials = renderer.sharedMaterials.ToList();
+
+            materials.Add(redGhostMaterial);
+            redAdd = true;
+
+            renderer.materials = materials.ToArray();
+        }
+        redAdd = true;
+    }
+    private void RemoveGhostEffets(Material m)
+    {
+        if (!redAdd && !blueAdd) { return; }
+        foreach (var renderer in renderers)
+        {
+            var materials = renderer.sharedMaterials.ToList();
+
+            materials.Remove(m);
+            if (m == blueGhostMaterial) { blueAdd = false; }
+            if (m == redGhostMaterial) { redAdd = false; }
+
+            renderer.materials = materials.ToArray();
+        }
+    }
+
+    //Collision Settings
+    private void MakeItemGhost()
+    {
+        Collider cd = CurrentItem.OriginalObject.GetComponent<Collider>();
+        cd.excludeLayers = ignoreLayer;
+        Rigidbody rb = CurrentItem.OriginalObject.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
+    private void MakeItemReal()
+    {
+        Collider cd = CurrentItem.OriginalObject.GetComponent<Collider>();
+        cd.excludeLayers = normLayer;
+    }
+
+    //Options Interactions with item
+    private void ItemDrop()
+    {
+        GameObject origObj = CurrentItem.OriginalObject;
+        if (origObj.GetComponent<Rigidbody>() == null)
+        {
+            Rigidbody rr = origObj.gameObject.AddComponent<Rigidbody>();
+            rr.mass = 1;
+            rr.angularDamping = 1.75f;
+        }
+        Rigidbody rb = origObj.GetComponent<Rigidbody>();
+        rb.mass = 1;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
+        rb.constraints = RigidbodyConstraints.None;
+        rb.angularDamping = 1.75f;
+        origObj.transform.position = PlayerCamera.transform.position;
+        origObj.SetActive(true);
+        rb.AddForce(PlayerCamera.transform.TransformDirection(Vector3.forward), ForceMode.Impulse);
+        RemoveItem(CurrentItem);
+    }
+    private void ItemPut()
+    {
+        SetItemNewPos(tempPos);
+        GameObject origObj = CurrentItem.OriginalObject;
+        origObj.transform.position = itemNewPos;
+        origObj.SetActive(true);
+    }
+
+    //Functional Position and Changhing items param
+    private void CreateBigWeight()
+    {
+        Rigidbody rg = CurrentItem.OriginalObject.GetComponent<Rigidbody>();
+    rg.linearVelocity = Vector3.zero;
+        rg.angularDamping = 50;
+        rg.constraints = RigidbodyConstraints.None;
+        rg.interpolation = RigidbodyInterpolation.Interpolate;
+    }
+    private void DetectionNewItemPos()
+    {
+        if (heldButton && timeButton > heldTime)
+        {
+            if (!_normalRotation) CurrentItem.OriginalObject.transform.rotation =
+        CurrentItem.OriginalObject.GetComponent<IPickUpInfo>().GetRotation();
+            _normalRotation = true;
+
+            MakeItemGhost();
+            if (putDistance < maxDistance)
+            {
+                if (redAdd) { RemoveGhostEffets(redGhostMaterial); }
+                PutGhostEffect();
+                ItemPut();
+            }
+            else
+            {
+                if (blueAdd) { RemoveGhostEffets(blueGhostMaterial); }
+                PutGhostEffectOutOfRange();
+                ItemPut();
+            }
+        }
+    }
+
+
+
+    private void SetItemNewPos(Vector3 h)
+    {
+        if (CurrentItem.OriginalObject.GetComponent<BoxCollider>() != null)
+        {
+            itemNewPos = new Vector3(
+            h.x,
+            h.y,
+            h.z + CurrentItem.OriginalObject.GetComponent<BoxCollider>().size.z / 2f
+
+            );
+        }
+        else if (CurrentItem.OriginalObject.GetComponent<CapsuleCollider>() != null)
+        {
+            itemNewPos = new Vector3(
+            h.x,
+            h.y + CurrentItem.OriginalObject.GetComponent<CapsuleCollider>().radius,
+            h.z
+            );
+        }
+        else if (CurrentItem.OriginalObject.GetComponent<SphereCollider>() != null)
+        {
+            itemNewPos = new Vector3(
+            h.x,
+            h.y + CurrentItem.OriginalObject.GetComponent<SphereCollider>().radius,
+            h.z
+            );
+        }
+    }
+
+    //Rotation Item
+    private void ItemRotate()
+    {
+        Rigidbody origObjrg = CurrentItem.OriginalObject.GetComponent<Rigidbody>();
+        origObjrg.interpolation = RigidbodyInterpolation.None;
+        origObjrg.constraints = RigidbodyConstraints.FreezeRotationY;
+        Transform origObjTrans = CurrentItem.OriginalObject.transform;
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            origObjTrans.Rotate(Vector3.up * Input.mouseScrollDelta.y * rotationIndex, Space.World);
+        }
+    }
+
+    //Final Realisation
+    private void PutAndDropItem()
+    {
+        RemoveGhostEffets(blueGhostMaterial);
+        RemoveGhostEffets(redGhostMaterial);
+        MakeItemReal();
+        timeButtonStart = false;
+        if (timeButton < heldTime)
+        {
+            itemDropped = true;
+            ItemDrop();
+            timeButton = 0;
+        }
+        else
+        {
+            if (putDistance < maxDistance)
+            {
+                if (!itemDropped)
+                {
+                    CreateBigWeight();
+                }
+                RemoveItem(CurrentItem);
+            }
+            else { Debug.Log("Too far"); CurrentItem.OriginalObject.SetActive(false); }
+            timeButton = 0;
+        }
+        itemDropped = false;
+        heldButton = false;
+    }
+    private void DropAndPutButton()
+    {
+        if (timeButtonStart)
+        {
+            timeButton += Time.deltaTime;
+        }
+
+        if (CurrentItem.gameObject == null) return;
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                timeButtonStart = true;
+                heldButton = true;
+
+            }
+            else if (Input.GetKeyUp(KeyCode.G))
+            {
+                PutAndDropItem();
+            }
+        }
+        ItemRayCast();
+        DetectionNewItemPos();
     }
 }
